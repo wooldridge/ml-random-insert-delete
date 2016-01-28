8e8a1b5e0eadbade7afc1cc7f0f72d56cd26ce20
Mike Wooldridge <mike@hellion.org>
2016-01-28 12:20:10 -0800

    Add time settings to config

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 var config = {};
 
+config.ratio = 1.0; // 1.0 all inserts, 0.0 all deletes
+config.pause = 5 // seconds
+config.padding = 2 // seconds
+
 config.marklogic = {
   host: 'localhost',
   port: '8000',
